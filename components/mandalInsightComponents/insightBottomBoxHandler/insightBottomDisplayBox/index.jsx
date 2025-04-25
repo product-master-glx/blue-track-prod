2b91d623ce02738fd5c3992ec40b8dab49a850f0
sudhit21 <sudhit.soni@galaxeye.space>
2025-04-25 18:40:20 +0530

    added toggle to bottom box

--- components/mandalInsightComponents/insightBottomBoxHandler/insightBottomDisplayBox/index.jsx
+++ components/mandalInsightComponents/insightBottomBoxHandler/insightBottomDisplayBox/index.jsx
@@ -1,50 +1,95 @@
-import { Box, Stack } from "@mui/material";
-import { useAtomValue } from "jotai";
+import { Box, Stack, IconButton, Slide } from "@mui/material";
+import { useAtom } from "jotai";
+import { useState } from "react";
 import PropTypes from "prop-types";
-import { graphFullSizeAtom } from "../../../../jotai";
+import { graphFullSizeAtom, mapScrollWheelZoomAtom } from "../../../../jotai";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 /**
- * This component wraps the bottom slider/monotone graph components
+ * This component wraps the bottom slider/monotone graph components with collapsable functionality
  */
 function InsightBottomDisplayBox({ children }) {
-	const GRAPH_FULL_SIZE_BOOLEAN = useAtomValue(graphFullSizeAtom);
+	const [isOpen, setIsOpen] = useState(true);
+	const [GRAPH_FULL_SIZE_BOOLEAN, ] = useAtom(graphFullSizeAtom);
+	const SET_MAP_SCROLL_WHEEL_ZOOM_BOOLEAN = useAtom(mapScrollWheelZoomAtom);
+
+	const handleToggle = () => {
+		const newState = !isOpen;
+		setIsOpen(newState);
+		// SET_GRAPH_FULL_SIZE_BOOLEAN(!newState);
+	};
+
+	const onMouseEnter = () => {
+		SET_MAP_SCROLL_WHEEL_ZOOM_BOOLEAN(false);
+	};
+
+	const onMouseLeave = () => {
+		SET_MAP_SCROLL_WHEEL_ZOOM_BOOLEAN(true);
+	};
 
 	return (
 		<>
-			<Box
-				component="div"
+			<IconButton
+				aria-label={isOpen ? "Close Bottom Box" : "Open Bottom Box"}
+				onClick={handleToggle}
 				sx={{
-					display: "flex",
-					justifyContent: "center",
-					width: GRAPH_FULL_SIZE_BOOLEAN ? "auto" : "calc(100% - 400px)",
-					p: 2,
+					p: "10px",
+					position: "fixed",
+					transition: "bottom 0.3s ease",
 					border: "1px solid grey",
-					borderRadius: "2",
+					borderRadius: "2px",
 					backgroundColor: "black",
-					position: "absolute",
-					right: 5,
-					left: 5,
-					bottom: 5,
-					zIndex: "1000",
-					marginRight: "auto",
+					zIndex: 1400,
+					marginBottom: "-20px",
+					transform: "translateY(-50%)",
+					bottom: isOpen ? "calc(33vh + 20px)" : "20px",
+					alignItems: "center",
+					right: "50%",
 				}}
 			>
-				<Stack
-					direction="row"
-					spacing={1}
-					alignItems="center"
+				{isOpen ? <KeyboardArrowDownIcon /> : <KeyboardArrowUpIcon />}
+			</IconButton>
+
+			<Slide direction="up" in={isOpen} mountOnEnter unmountOnExit>
+				<Box
+					component="div"
 					sx={{
 						display: "flex",
-						width: "100%",
-						height: "100%",
-						overflowY: "none",
-						margin: "5px",
-						mb: "0px",
+						justifyContent: "center",
+						width: GRAPH_FULL_SIZE_BOOLEAN ? "auto" : "calc(100% - 412px)",
+						p: 2,
+						border: "1px solid grey",
+						borderRadius: "2px",
+						backgroundColor: "black",
+						position: "fixed",
+						right: "5px",
+						left: "5px",
+						bottom: "5px",
+						zIndex: 1300,
+						marginRight: "auto",
+						height: "33vh",
 					}}
+					onMouseEnter={onMouseEnter}
+					onMouseLeave={onMouseLeave}
 				>
-					{children}
-				</Stack>
-			</Box>
+					<Stack
+						direction="row"
+						spacing={1}
+						alignItems="center"
+						sx={{
+							display: "flex",
+							width: "100%",
+							height: "100%",
+							margin: "5px",
+							mb: "0px",
+							overflowY: "hidden",
+						}}
+					>
+						{children}
+					</Stack>
+				</Box>
+			</Slide>
 		</>
 	);
 }
